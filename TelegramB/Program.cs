1f5d7d0e90aba239b830bc67eeb2f08403a8dee6
Mark <koly86@gmail.com>
2023-09-22 10:27:53 +0300

    First Commit

---
+++ TelegramB/Program.cs
@@ -0,0 +1,109 @@
+﻿
+using System.Globalization;
+using RestSharp;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TelegramB;
+
+
+string token = "";
+string url = $"https://api.telegram.org/bot{token}";
+TelegramBotClientOptions options = new TelegramBotClientOptions(token: token, baseUrl: url);
+
+var botClient = new TelegramBotClient(options:options);
+
+using CancellationTokenSource cts = new();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    string userMessage = "hello";
+
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { Text: { } messageText } message)
+        return;
+    // Only process text messages
+
+    var chatId = message.Chat.Id;
+    Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+    
+    switch (messageText.ToLower())
+    {
+        case "время":
+            userMessage = DateTime.Now.TimeOfDay.ToString("hh\\:mm");
+            // Echo received message text
+            await botClient.SendTextMessageAsync(
+               chatId: chatId,
+               text: userMessage,
+               cancellationToken: cancellationToken);
+            break;
+        case "дата":
+            userMessage = DateTime.Now.Date.ToShortDateString();
+            // Echo received message text
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: userMessage,
+                cancellationToken: cancellationToken);
+            break;
+        case "скучно":
+            userMessage = "скучно только пидорам";
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: userMessage,
+                cancellationToken: cancellationToken);
+            break;
+        case "погода":
+
+            Task<string> res = new Weather(new RestClient("http://api.weatherapi.com/v1")).GetWeather();
+
+            userMessage = res.Result + "°C";
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: userMessage,
+                cancellationToken: cancellationToken);
+            break;
+
+        default:
+            userMessage = ".";
+            break;
+    }
+
+
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
---
+++ TelegramB/Weather.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using RestSharp;
+
+namespace TelegramB
+{
+    internal class Weather
+    {
+        private readonly RestClient client;
+        //Key: 6a81939f30034d66a75135404232109
+        public Weather(RestClient client)
+        {
+            this.client = client;
+        }
+
+        internal async Task<string> GetWeather()
+        {
+            var request = new RestRequest("/current.json", Method.Get);
+            request.AddParameter("key", "6a81939f30034d66a75135404232109");
+            request.AddParameter("q", "Kudrovo");
+            RestResponse data = await client.ExecuteAsync(request);
+            string Temp = string.Empty;
+            if (data.Content == null) return Temp;
+            dynamic array = JsonConvert.DeserializeObject(data.Content);
+#if DEBUG // TODO доделать погоду
+            Console.WriteLine(array is IJEnumerable<JToken>);
+            Console.WriteLine(array.GetType());
+            Console.WriteLine(array is JProperty);
+
+            var enumerable = (JObject)array;
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+            JProperty property = enumerator.Current as JProperty;
+            Console.WriteLine(property.Name, property.Value, property.Type);
+#endif
+
+            foreach (Newtonsoft.Json.Linq.JProperty prop in array)
+            {
+                if (prop.Name == "current")
+                {
+                    var current_Data = prop.Value.Children();
+                    foreach (Newtonsoft.Json.Linq.JProperty item in current_Data)
+                    {
+                        switch (item.Name)
+                        {
+                            case "temp_c":
+                                Temp = item.Value.ToString();
+                                break;
+                        }
+                    }
+                }
+            }
+            return Temp;
+        }
+    }
+}
